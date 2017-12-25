6b73749f60ba97fce26605b88b5c8eb341079c29
ecourse <smkang@codenuri.co.kr>
2017-12-26 00:30:23 +0900

    Add files via upload

---
+++ 0000_ORIENTATION/hello.cpp
@@ -0,0 +1,15 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+/* HOME  : ecourse.co.kr
+/* EMAIL : smkang @ codenuri.co.kr 
+/* COPYRIGHT 2018. CODENURI CORP. ALL RIGHTS RESERVED.
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+// hello.cpp
+#include <stdio.h>
+
+int main()
+{
+	printf("hello, C++\n");
+}
+
